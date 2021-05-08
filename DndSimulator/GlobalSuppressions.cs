2cd1ce56018250ccac528daf8dceb8d0bc3e89b1
Zachary Hatton <zachattackhatton@gmail.com>
2021-05-07 21:08:52 -0700

    Warning clean up

---
+++ DndSimulator/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Parameter 'req' is necessary to Azure Function", Scope = "member", Target = "~M:DndSimulator.Api.Functions.HelloWorldFunction.HelloWorld(Microsoft.AspNetCore.Http.HttpRequest,Microsoft.Extensions.Logging.ILogger)~Microsoft.AspNetCore.Mvc.IActionResult")]
