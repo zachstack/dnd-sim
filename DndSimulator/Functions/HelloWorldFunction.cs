b73d5cb24d7c30c75956bcde6056d559921d998e
Zachary Hatton <zachattackhatton@gmail.com>
2021-05-09 12:36:13 -0700

    Added OpenAPi/Swagger documentation

--- DndSimulator/Functions/HelloWorldFunction.cs
+++ DndSimulator/Functions/HelloWorldFunction.cs
@@ -1,17 +1,19 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Net;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
 
 namespace DndSimulator.Api.Functions
 {
     public class HelloWorldFunction
     {
+        [OpenApiOperation(operationId: "HelloWorld", Summary = "Gets the string \"Hello World!\"", Description = "Gets the string \"Hello World!\"", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "Returns the string \"Hello World!\"")]
         [FunctionName("HellowWorld")]
         public IActionResult HelloWorld(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello-world")] HttpRequest req,
@@ -19,7 +21,7 @@ namespace DndSimulator.Api.Functions
         {
             log.LogInformation("Hello World!");
 
-            string message = "Hello World! Sike!";
+            string message = "Hello World!";
 
             return new OkObjectResult(message);
         }
